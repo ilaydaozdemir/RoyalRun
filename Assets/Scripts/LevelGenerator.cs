4ac1a6d3ef5060f12da132f8233e7747c22c6255
ozdemirilayda <ozdemirilayda95@gmail.com>
2026-04-21 14:47:14 +0300

    Chunk Movement

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -6,24 +6,51 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] int startingChunkAmount=12; 
     [SerializeField] Transform chunkParent;
     [SerializeField]float chunkLenght=10f;
+    [SerializeField] float moveSpeed=8f;
+    GameObject[] chuncks=new GameObject[12];
     void Start()
+    {
+        SpawnChunks();
+
+    }
+    void Update()
+    {
+        MoveChunks();
+    }
+
+     void SpawnChunks()
     {
         for (int i = 0; i < startingChunkAmount; i++)
         {
-            float spawnPositionZ;
-            
-            if(i==0)
-            {
-                spawnPositionZ=transform.position.z;
-            }
-            else
-            {
-                spawnPositionZ=transform.position.z+(i*chunkLenght);
-            }
-            Vector3 chunkSpawnPos=new Vector3(transform.position.x,transform.position.y,spawnPositionZ);
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
+            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            GameObject newChunk= Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
 
-            Instantiate(chunkPrefab,chunkSpawnPos,Quaternion.identity,chunkParent);
+            chuncks[i]=newChunk;
         }
-        
+    }
+
+     float CalculateSpawnPositionZ(int i)
+    {
+        float spawnPositionZ;
+
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i * chunkLenght);
+        }
+
+        return spawnPositionZ;
+    }
+
+   void MoveChunks()
+    {
+        for (int i = 0; i < chuncks.Length; i++)
+        {
+            chuncks[i].transform.Translate(-transform.forward*(moveSpeed*Time.deltaTime));
+        }
     }
 }
