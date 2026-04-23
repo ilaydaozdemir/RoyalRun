029805d6876d20120e0f75838c08d3466debc2f5
ozdemirilayda <ozdemirilayda95@gmail.com>
2026-04-23 20:36:59 +0300

    add score manager

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Coin : Pickup
 {
+    ScoreManager scoreManager;
+    void Start()
+    {
+        scoreManager=FindFirstObjectByType<ScoreManager>();
+    }
     protected override void OnPickup()
     {
-       
+       scoreManager.IncreaseScore(100);
     }
 }
